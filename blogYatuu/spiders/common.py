4760e02ed54851d3b502a641f6cb8b1e9e49333c
sasquatch <16246296+SasquatchYuja@users.noreply.github.com>
2020-10-23 06:18:13 +0200

    common: added setEnv()

--- blogYatuu/spiders/common.py
+++ blogYatuu/spiders/common.py
@@ -11,3 +11,23 @@ from string import Template as model
 def init(env):
     setEnv(env)
     bootstrap(env)
+
+
+def setEnv(env):
+    baseURL = f"https://{env.allowed_domains[0]}"
+
+    env.begURL = f"{baseURL}/{env.begURL}"
+    env.endURL = f"{baseURL}/{env.endURL}"
+
+    env.start_urls.append(env.begURL)
+
+    env.extendedPattern = f"^{baseURL}/wp-content/uploads/(((pages-post-)?erikaa?|nv)[_-]?)?([0-9]{{1,3}}(-([137]{{2,3}}|suite|OUI))?)([_-]?(c?oo?k(ay|ii?|k)?){{1,3}})?(-[12])?([_-]?(bis|tris))?\.(jpe?g|png)$"
+    env.S = "\033[1;31m" # set color to bold red
+    env.R = "\033[0m"    # reset color to normal
+
+    env.outDir = "./out"
+    env.tDir = f"{env.outDir}/{env.tDir}"
+    env.urlFile = f"{env.outDir}/{env.urlFile}"
+
+    env.nbPages = 0
+    env.totalCptImgs = 0 # total number of downloaded images
