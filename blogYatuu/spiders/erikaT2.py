de3ff9cd839e1f5ce4c51574c174c3017fa49a2a
sasquatch <16246296+SasquatchYuja@users.noreply.github.com>
2020-10-23 06:38:33 +0200

    spiders: t2 : replicating t1's spider's changes

--- blogYatuu/spiders/erikaT2.py
+++ blogYatuu/spiders/erikaT2.py
@@ -9,6 +9,18 @@ class Erikat2Spider(scrapy.Spider):
     allowed_domains = ['yatuu.fr']
     start_urls = []
 
+    begURL = "1-erika-et-les-princes-en-detresse-tome-2/" # https://yatuu.fr/1-erika-et-les-princes-en-detresse-tome-2/
+    endURL = "47-erika-et-les-princes-en-detresse-fin-tome-2/" # https://yatuu.fr/47-erika-et-les-princes-en-detresse-fin-tome-2/
+
+    tDir = "Erika-T2"
+    urlFile = "URListT2.txt"
+
+    boolLogFailedRegexes = False
+
+
+    def __init__(self):
+        common.init(self)
+
 
     def parse(self, response):
-        pass
+        return common.parse(self, response)
