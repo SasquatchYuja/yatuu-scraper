e2805f2a1e3623e0e7c3215725e06288ff15b6a3
sasquatch <16246296+SasquatchYuja@users.noreply.github.com>
2020-10-23 06:34:03 +0200

    spiders: t1 : added start/end URLs

--- blogYatuu/spiders/erikaT1.py
+++ blogYatuu/spiders/erikaT1.py
@@ -9,6 +9,10 @@ class Erikat1Spider(scrapy.Spider):
     allowed_domains = ['yatuu.fr']
     start_urls = []
 
+    begURL = "erika-et-les-princes-en-detresse-page-1-a-4/" # https://yatuu.fr/erika-et-les-princes-en-detresse-page-1-a-4/
+    endURL = "erika-et-les-princes-en-detresse-82/" # https://yatuu.fr/erika-et-les-princes-en-detresse-82/
+    #begURL = "erika-princes-detresse-67/"
+    #endURL = "erika-princes-detresse-67/"
 
     def parse(self, response):
         pass
